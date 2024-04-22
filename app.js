e86795563b169527ba882d76d4b7fd4bc82321bc
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-22 10:39:54 +0500

    added

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let firstName = prompt("Can you enter your name?");-9
 let firstLetter = firstName.charAt(0).toUpperCase();
 let restOfName = firstName.slice(1).toLowerCase();
 let result = firstLetter + restOfName;
-alert(`Assalamu alaykum,Dear ${result} NTMU`)
+alert(`Assalamu alaykum,Dear ${result} Nice to meet you`)
